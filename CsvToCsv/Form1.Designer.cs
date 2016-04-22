0759c74f0dffb288ae7316669725cfc7f43458e9
mjimenez2014 <jimenezmaury@gmail.com>
2016-04-22 13:38:36 -0300

    CommitInicial

---
+++ CsvToCsv/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace CsvToCsv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonGeneraCsv = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonAbrirArchivo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxRutReceptor = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.96791F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonGeneraCsv, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.60714F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.39286F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(857, 489);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(851, 320);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // buttonGeneraCsv
+            // 
+            this.buttonGeneraCsv.Location = new System.Drawing.Point(3, 396);
+            this.buttonGeneraCsv.Name = "buttonGeneraCsv";
+            this.buttonGeneraCsv.Size = new System.Drawing.Size(86, 23);
+            this.buttonGeneraCsv.TabIndex = 3;
+            this.buttonGeneraCsv.Text = "Genera";
+            this.buttonGeneraCsv.UseVisualStyleBackColor = true;
+            this.buttonGeneraCsv.Click += new System.EventHandler(this.buttonGeneraCsv_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 42.19858F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 57.80142F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 437F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 21F));
+            this.tableLayoutPanel2.Controls.Add(this.buttonAbrirArchivo, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBoxRutReceptor, 2, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(699, 36);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // buttonAbrirArchivo
+            // 
+            this.buttonAbrirArchivo.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonAbrirArchivo.Location = new System.Drawing.Point(3, 6);
+            this.buttonAbrirArchivo.Name = "buttonAbrirArchivo";
+            this.buttonAbrirArchivo.Size = new System.Drawing.Size(75, 23);
+            this.buttonAbrirArchivo.TabIndex = 1;
+            this.buttonAbrirArchivo.Text = "Abrir Archivo";
+            this.buttonAbrirArchivo.UseVisualStyleBackColor = true;
+            this.buttonAbrirArchivo.Click += new System.EventHandler(this.buttonAbrirArchivo_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(104, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Rut Receptor: ";
+            // 
+            // textBoxRutReceptor
+            // 
+            this.textBoxRutReceptor.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxRutReceptor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxRutReceptor.Location = new System.Drawing.Point(243, 7);
+            this.textBoxRutReceptor.Name = "textBoxRutReceptor";
+            this.textBoxRutReceptor.Size = new System.Drawing.Size(97, 22);
+            this.textBoxRutReceptor.TabIndex = 3;
+            this.textBoxRutReceptor.Text = "00000000-0";
+            this.textBoxRutReceptor.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 489);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonAbrirArchivo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonGeneraCsv;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxRutReceptor;
+    }
+}
+
---
+++ CsvToCsv/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CsvToCsv
+{
+    public partial class Form1 : Form
+    {
+        List<string[]> parsedata = new List<string[]>();
+        List<string[]> copias = new List<string[]>();
+        string[] copia;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string formatFecha(string fecha)
+        {
+            fecha = Convert.ToDateTime(fecha).ToString("yyyy-MM-dd");
+            return fecha;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private string buscaRepetido(string folio, string tipoDte, string rutEmis)
+        {
+            string existe = "False";
+            foreach (string[] row in copias)
+            {
+                if (row[1] == folio && row[0] == tipoDte && row[2] == rutEmis)
+                {
+                     existe = "True";
+                }
+            }
+            return existe;
+        }
+
+        private string getLineadosImp(string folio, string tipoDte, string rutEmis)
+        {
+            string[] lista;
+            string linea = string.Empty;
+            List<string[]> listas = new List<string[]>();
+            int conteo = 0;
+            foreach (string[] row in parsedata)
+            {
+                if (row[1] == folio && row[0] == tipoDte && row[2] == rutEmis)
+                {
+                    conteo += 1;
+                    lista = row;
+                    listas.Add(lista);
+                    string impto18 = "0";
+                    if (conteo > 1)
+                    {
+
+                        foreach (string[] rowCopia in listas)
+                        {
+                            string fecha = formatFecha(rowCopia[6]);
+                            string impto10 = "0";
+                            string impto25 = "0";
+                            string impto30 = "0";
+                            string rutemisor = rowCopia[2];
+                            if (rowCopia[15] == "18") impto18 = rowCopia[16];
+                            if (rowCopia[15] == "10") impto10 = rowCopia[16];
+                            if (rowCopia[15] == "25") impto10 = rowCopia[16];
+                            if (rowCopia[15] == "30") impto10 = rowCopia[16];
+                            if (rowCopia[8] == "") rowCopia[8] = "0";
+                            if (rowCopia[9] == "") rowCopia[9] = "0";
+                            if (rowCopia[10] == "") rowCopia[10] = "0";
+                            if (row[11] == "") row[11] = "0";
+                            if (row[12] == "") row[12] = "0";
+                            Console.WriteLine(rowCopia[0] + ";" + rowCopia[1] + ";" + rowCopia[2] + ";" + rowCopia[16]);
+                            if (rowCopia[15] == "18")
+                            {
+                                impto18 = rowCopia[16];
+                            }
+                            else
+                            {
+                                linea = (rowCopia[0] + ";"
+                                     + rowCopia[1] + ";"
+                                     + fecha + ";"
+                                     + rutemisor + ";"
+                                     + textBoxRutReceptor.Text + ";"
+                                     + rowCopia[4] + ";"
+                                     + rowCopia[9] + ";"
+                                     + rowCopia[8] + ";"
+                                     + rowCopia[10] + ";"
+                                     + "0" + ";"
+                                     + rowCopia[28] + ";"
+                                     + impto18 + ";"
+                                     + impto10 + ";"
+                                     + impto25 + ";"
+                                     + impto30 + ";"
+                                     + "PREVIO" + ";"
+                                     + row[12] + ";"
+                                     + row[11]
+                                     );
+                                copia = rowCopia;
+                            }
+                        }
+                    }
+                }
+
+
+            }
+            return linea;
+
+        }
+
+        private void buttonAbrirArchivo_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = this.openFileDialog1.FileName;
+            }
+            this.openFileDialog1.Dispose();
+
+            using (var sr = new StreamReader(@"" + label1.Text + "", Encoding.Default, true))
+            {
+                string line = "";
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    line = line.Replace("Ñ", "N");
+                    line = line.Replace("&", "Y");
+                    string[] row = line.Split(';');
+                    Console.WriteLine(row[2]);
+                    parsedata.Add(row);
+
+                }
+            }
+            dataGridView1.ColumnCount = 29;
+            for (int i = 0; i < 29; i++)
+            {
+                var sb = new StringBuilder(parsedata[0][i]);
+                sb.Replace('_', ' ');
+                sb.Replace("\"", "");
+                dataGridView1.Columns[i].Name = sb.ToString();
+
+            }
+
+            foreach (string[] row in parsedata)
+            {
+                dataGridView1.Rows.Add(row);
+            }
+            dataGridView1.Rows.Remove(dataGridView1.Rows[0]);
+
+        }
+
+        private void buttonGeneraCsv_Click(object sender, EventArgs e)
+        {
+            string fchActual = DateTime.Now.ToString("yyyyMMddhhmmss");
+            string dirDestino = this.openFileDialog1.FileName;
+            dirDestino = dirDestino.Replace(".CSV", "_Procesado.CSV");
+            StreamWriter escribe = new StreamWriter(@"" + dirDestino);
+            escribe.WriteLine("tipodoc;folio;fchemis;rutemisor;rutrecep;rznsocemisor;mntneto;mntexe;mntiva;otrosimpto;mnttotal;impto18;impto10;impto25;impto30;estado;ivanorec;codivanorec");
+
+            foreach (string[] row in parsedata)
+            {
+                // elimino la primera linea
+                if (row[0] != "Tipo Doc")
+                {
+                    //doy formato a la fecha y declaro variables
+                    string fecha = formatFecha(row[6]);
+                    string impto18 = "0";
+                    string impto10 = "0";
+                    string impto25 = "0";
+                    string impto30 = "0";
+                    string rutemisor = row[2];
+                    if (row[2].Length < 10) rutemisor = row[2].Insert(0, "0");
+                    //cargo la fila
+                    // si tasa de impuesto es 18(row[15]) monto es igual a impto18
+                    if (row[15] == "18") impto18 = row[16];
+                    if (row[15] == "10") impto10 = row[16];
+                    if (row[15] == "25") impto10 = row[16];
+                    if (row[15] == "30") impto10 = row[16];
+                    if (row[8] == "") row[8] = "0";
+                    if (row[9] == "") row[9] = "0";
+                    if (row[10] == "") row[10] = "0";
+                    if (row[11] == "") row[11] = "0";
+                    if (row[12] == "") row[12] = "0";
+
+                    if (getLineadosImp(row[1], row[0], row[2]) != "")
+                    {
+                        if (buscaRepetido(row[1], row[0], row[2]) == "False")
+                        {
+                            escribe.WriteLine(getLineadosImp(row[1], row[0], row[2]));
+                            copias.Add(copia);
+                            copia.Initialize();
+                        }
+
+                    }
+                    else
+                    {
+
+                        escribe.WriteLine(row[0] + ";"
+                                        + row[1] + ";"
+                                        + fecha + ";"
+                                        + rutemisor + ";"
+                                        + textBoxRutReceptor.Text + ";"
+                                        + row[4] + ";"
+                                        + row[9] + ";"
+                                        + row[8] + ";"
+                                        + row[10] + ";"
+                                        + "0" + ";"
+                                        + row[28] + ";"
+                                        + impto18 + ";"
+                                        + impto10 + ";"
+                                        + impto25 + ";"
+                                        + impto30 + ";"
+                                        + "PREVIO" + ";"
+                                        + row[12] + ";"
+                                        + row[11]
+                                        );
+
+                    }
+
+                }
+
+            }
+            escribe.Close();
+            MessageBox.Show("Archivo Generado en: " + dirDestino);
+            this.Close();
+        }
+
+    }
+}
